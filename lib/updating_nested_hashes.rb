1f4d1424a1cf0d81181dd520f467c856c063196c
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 13:34:24 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,7 +61,7 @@ def add_characters
    # Code your solution here:
   
       :additional_characters => [
-        :name => "Prince Escalus"
+        :name => "Prince Escalus",
         :name => "Apothecary"
         ]
      }
